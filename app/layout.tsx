2c3b97d22bd2b755f4f95c6a51114cb5bf7082dd
messams <sir.m.essam.g@gmail.com>
2025-04-26 09:12:19 +0300

    add: about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <head>
         <link rel="icon" href="/printforge-logo-icon.svg" />
       </head>
