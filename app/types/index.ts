433dfc381a5d58610c9a70d3c79e10d49aeb5d78
messams <sir.m.essam.g@gmail.com>
2025-04-26 10:42:20 +0300

    our app types

---
+++ app/types/index.ts
@@ -0,0 +1,32 @@
+import type { ReactNode } from "react"
+
+export type Model = {
+    id: number
+    name: string
+    description: string
+    likes: number
+    image: string
+    category: string
+    dateAdded: string
+}
+
+// Page Types
+export type RootLayoutProps = Readonly<{
+    children: React.ReactNode;
+}>
+
+export type ModelDetailPageProps = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+// Components Types
+export type ModelCardProps = {
+    model: Model
+}
+
+export type PillProps = {
+    children: ReactNode
+    className?: string
+}
