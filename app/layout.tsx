3fef1d9daa96bd514261cf0da304c65d3378a2f3
messams <sir.m.essam.g@gmail.com>
2025-04-26 10:41:50 +0300

    call the Navbar compoent

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
-import Image from "next/image";
-import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -33,38 +32,7 @@ export default function RootLayout({
         <link rel="icon" href="/printforge-logo-icon.svg" />
       </head>
       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-            <Link href="/">
-              <div className="relative">
-                {/* Desktop logo */}
-                <Image
-                  width={200}
-                  height={200}
-                  src="/printforge-logo.svg"
-                  alt="PrintForge Logo"
-                  className="w-[200px] h-auto hidden md:block"
-                />
-                {/* Mobile logo */}
-                <Image
-                  width={40}
-                  height={40}
-                  src="/printforge-logo-icon.svg"
-                  alt="PrintForge Logo"
-                  className="w-[40px] h-auto block md:hidden"
-                />
-              </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/3d-models">3D Models</Link>
-              </li>
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Navbar />
         {children}
       </body>
     </html>
