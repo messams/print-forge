fb6d9f7783af19fd0d8826a4e395956cf247da00
messams <sir.m.essam.g@gmail.com>
2025-04-26 09:32:58 +0300

    Adding: Links to Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
 import Image from "next/image";
+import Link from "next/link";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -34,27 +35,33 @@ export default function RootLayout({
       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
-            <div className="relative">
-              {/* Desktop logo */}
-              <Image
-                width={200}
-                height={200}
-                src="/printforge-logo.svg"
-                alt="PrintForge Logo"
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile logo */}
-              <Image
-                width={40}
-                height={40}
-                src="/printforge-logo-icon.svg"
-                alt="PrintForge Logo"
-                className="w-[40px] h-auto block md:hidden"
-              />
-            </div>
+            <Link href="/">
+              <div className="relative">
+                {/* Desktop logo */}
+                <Image
+                  width={200}
+                  height={200}
+                  src="/printforge-logo.svg"
+                  alt="PrintForge Logo"
+                  className="w-[200px] h-auto hidden md:block"
+                />
+                {/* Mobile logo */}
+                <Image
+                  width={40}
+                  height={40}
+                  src="/printforge-logo-icon.svg"
+                  alt="PrintForge Logo"
+                  className="w-[40px] h-auto block md:hidden"
+                />
+              </div>
+            </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p>About</p>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/3d-models">3D Models</Link>
+              </li>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
             </ul>
           </nav>
         </header>
